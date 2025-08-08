b15259024c6bfe7c260dbde581436f816f7c6f06
TiagoSoutoMA <tiagosoutoma@gmail.com>
2025-08-08 18:17:31 -0300

    feat: adicionado ao projeto o allure report para geração de relatórios após a execução dos testes

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,17 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/features/**/*.feature",
     baseUrl: "http://localhost:3000",
     watchForFileChanges: false,
+    env: {
+      allureReuseAfterSpec: true,
+      allure: true,
+    },
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
       on(
@@ -16,6 +21,7 @@ module.exports = defineConfig({
           plugins: [createEsbuildPlugin(config)],
         })
       );
+      allureWriter(on, config);
       return config;
     },
   },
